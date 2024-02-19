a0002074fc3c2905af40d5497261263f14c1622f
DragosU25 <149568004+DragosU25@users.noreply.github.com>
2024-02-19 18:56:13 +0000

    Deploying to gh-pages from @ DragosU25/team-project@f5248452f0b2b0710d495b3196c082e1fcbe0580 🚀

---
+++ index.703ac527.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[thanks-modal-open]"),closeModalBtn:document.querySelector("[thanks-modal-close]"),modal:document.querySelector("[thanks-modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.703ac527.js.map
